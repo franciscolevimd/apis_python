384bb12acdd85ad32a3e17ac561659a7524ede7e
Francisco Leví <francisco.levi.md@gmail.com>
2021-04-07 21:48:06 -0500

    List the pokemon names.

--- pokeapi/pokedex.py
+++ pokeapi/pokedex.py
@@ -1,5 +1,44 @@
-def display_settings():
-	pass
+import requests
+
+
+URL = 'https://pokeapi.co/api/v2/pokemon'
+
+
+def get_pokemons(limit=10, offset=0):
+	parameters = {
+					'limit':limit, 
+					'offset':offset
+				 }
+	response = requests.get(URL, params=parameters)
+	if response.status_code == 200:
+		return [pokemon.get('name') for pokemon in response.json().get('results', [])]
+	else:
+		print(f'Status code: {response.status_code}')
+
+
+def list_pokemons():		
+	get_pokemons()
+	limit = 10
+	offset = 0
+	page = 1
+	pokemons = get_pokemons()
+	while True:
+		for pokemon in pokemons:
+			print(f'\t{pokemon}')
+		print(f'\n[P]revious | p[A]ge {page} | [S]how {limit} pokemons | [N]ext | [B]ack to menu')
+		option = input('# Select an option: ').upper()
+		if option == 'P':
+			print('Previous')
+		elif option == 'A':
+			print('Page')
+		elif option == 'S':
+			print('Limit')
+		elif option == 'N':
+			print('Next')		
+		elif option == 'B':
+			break
+		else:
+			print('Invalid option!')
 
 
 def display_menu():
@@ -15,13 +54,16 @@ def display_menu():
 def main():
 	while True:
 		display_menu()
-		option = input('Select an option: ').upper()
+		option = input('# Select an option: ').upper()
 		if option == 'L':
-			print('List pokemons')
+			print('')
+			list_pokemons()
 		elif option == 'F':
 			print('Find pokemons')
 		elif option == 'E':
 			break
+		else:
+			print('Invalid option!')
 
 
 if __name__ == '__main__':
