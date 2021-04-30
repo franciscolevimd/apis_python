85fd646e81b91b1ed2d7165af3ee43ee2268833d
Francisco Leví <francisco.levi.md@gmail.com>
2021-04-29 22:28:34 -0500

    Refactorization in the service of the pokedex.

--- pokeapi/service.py
+++ pokeapi/service.py
@@ -1,4 +1,5 @@
 import requests
+import re
 
 
 URL = 'https://pokeapi.co/api/v2/pokemon'
@@ -6,28 +7,220 @@ CODE_200 = 200
 
 
 def get_pokemon_header(url):
+	"""Retrieves the basic data of a pokemon: id, name and types.
+
+	Args:
+		url (str) - Request of data to the pokeapi.
+
+	Returns:
+		dict: Basic data of the pokemon: 
+		{
+			'id':13,
+			'name':'Weedle',
+			'types':['Bug', 'Poison']
+		}
+	"""
 	response = requests.get(url)
 	if response.status_code == CODE_200:
 		pokemon_json = response.json()
 		pokemon = {'id':pokemon_json.get('id'), 'name':pokemon_json.get('name').capitalize()}
 		pokemon['types'] = [type_json.get('type').get('name').capitalize() for type_json in pokemon_json.get('types', [])]
 		return pokemon
-	else:
-		print(f'Status code: {response.status_code}')
 
 
-def get_pokemons(limit=10, offset=0):
-	parameters = {
-		'limit':limit, 
-		'offset':offset
-	}
-	response = requests.get(URL, params=parameters)
+def get_data_paged(pokemons_by_page):
+	"""Retrieves the information of paged.
+
+	Args:
+		pokemons_by_page (int) - Limit of pokemons by page.
+
+	Returns:
+		dict - Data of paged: 
+		{
+			'count_pokemons':1118,			
+			'pokemons_by_page':10,
+			'count_pages':112
+		}
+	"""
+	response = requests.get(URL)
+	if response.status_code == CODE_200:		
+		pokemons_json = response.json()		
+		count_pokemons = int(pokemons_json.get('count'))
+		count_pages = (count_pokemons // pokemons_by_page) + 1 if count_pokemons % pokemons_by_page > 0 else count_pokemons // pokemons_by_page		
+		return {'count_pokemons':count_pokemons, 'pokemons_by_page':pokemons_by_page, 'count_pages':count_pages}
+
+
+def get_page_number(count_pages, url):
+	"""Generates the page number from the URL.
+
+	Args:
+		count_pages (int) - Total of pages.
+		url (str) - Url to retrieves the pokemons page.
+
+	Returns: 
+		int - Page number.
+	"""
+	pattern = re.compile(r'^.+offset=(\d+)&limit=(\d+)$')
+	response = re.match(pattern, url)
+	if response:
+		pokemons_by_page = int(response.group(2))
+		offset = int(response.group(1))
+		return (offset // pokemons_by_page) + 1
+
+
+
+def get_page_parameters(data_paged, page_number):
+	"""Retrieves the parameters to get a page by URL.
+
+	Args:
+		data_paged (dict) - Contains total of pages and the number of pokemons by page -count_pages, pokemons_by_page-.
+							See get_data_paged().
+		page_number (int)
+
+	Returns:
+		dict - Parameters to the url: 
+		{
+			'limit':20,
+			'offset':150
+		}
+	"""
+	offset = 0
+	count_pages = data_paged.get('count_pages')		
+	pokemons_by_page = data_paged.get('pokemons_by_page')
+	if page_number > count_pages:
+		offset = (count_pages * pokemons_by_page) - pokemons_by_page
+	elif 0 < page_number <= count_pages:		
+		offset = pokemons_by_page * (page_number - 1)
+	return {'limit':pokemons_by_page, 'offset':offset}
+
+
+def get_content_page(url, parameters=None):
+	"""Retrieves the list of pokemons and the next and previous url page. 
+
+	Args:
+		url (str) - URL of the page. 
+		parameters (dict, optional) - Contains the parameter to request the pokemon page. See requests.get() 
+
+	Returns:
+		dict - Content page: 
+		{
+			'next':'next":"https://pokeapi.co/api/v2/pokemon?offset=1115&limit=3',
+			'previous':'https://pokeapi.co/api/v2/pokemon?offset=1105&limit=5'
+			'pokemons':[
+				{
+					'id': 10213, 
+					'name': 'Grimmsnarl-gmax', 
+					'types': ['Dark', 'Fairy']
+				}, 
+				{
+					'id': 10214, 
+					'name': 'Alcremie-gmax', 
+					'types': ['Fairy']
+				}, 
+				{
+					'id': 10215, 
+					'name': 'Copperajah-gmax', 
+					'types': ['Steel']
+				}, 
+				{
+					'id': 10216, 'name': 
+					'Duraludon-gmax', 
+					'types': ['Steel', 'Dragon']
+				}, 
+				{
+					'id': 10217, 
+					'name': 'Eternatus-eternamax', 
+					'types': ['Poison', 'Dragon']
+				}
+			]
+		}
+	"""
+	response = requests.get(url, parameters)
 	if response.status_code == CODE_200:
-		pokemon_json = response.json()
-		return [get_pokemon_header(pokemon_data.get('url')) for pokemon_data in pokemon_json.get('results', [])]
-	else:
-		print(f'Status code: {response.status_code}')
+		content_page = response.json()	
+		content_page.pop('count')
+		pokemons = content_page.pop('results')
+		content_page['pokemons'] = [get_pokemon_header(pokemon.get('url')) for pokemon in pokemons]			
+		return content_page
 
+
+def get_page(url, data_paged, page_number=None):
+	"""Retrieves a page with its pokemons list. 
+
+	Args:
+		url (str) - URL of the page. 
+		data_paged (dict) - Contains total of pages and the number of pokemons by page -count_pages, pokemons_by_page-.
+							See get_data_paged().
+		page_number (int, optional)
+
+	Returns:
+		dict - Page: 
+		{
+			'number': 20, 
+			'offset': 95, 
+			'next': 'https://pokeapi.co/api/v2/pokemon?offset=100&limit=5', 
+			'previous': 'https://pokeapi.co/api/v2/pokemon?offset=90&limit=5', 
+			'content': [
+				{
+					'id': 96, 
+					'name': 'Drowzee', 
+					'types': ['Psychic']
+				}, 
+				{
+					'id': 97, 
+					'name': 'Hypno', 
+					'types': ['Psychic']
+				}, 
+				{
+					'id': 98, 
+					'name': 'Krabby', 
+					'types': ['Water']
+				}, 
+				{
+					'id': 99, 
+					'name': 'Kingler', 
+					'types': ['Water']
+				}, 
+				{
+					'id': 100, 
+					'name': 'Voltorb', 
+					'types': ['Electric']
+				}
+			]
+		}		
+	"""
+	page = None
+	content_page = None
+	
+	if page_number and data_paged:
+		content_page = get_content_page (
+				url, 
+				get_page_parameters(data_paged, page_number)
+			)
+	elif url:		
+		content_page = get_content_page(url)
+		page_number = get_page_number(data_paged.get('count_pages'), url)
+
+	if content_page and content_page.get('pokemons'):
+		if page_number == data_paged.get('count_pages'):
+			content_page['next'] = f'https://pokeapi.co/api/v2/pokemon?offset=0&limit={data_paged.get("pokemons_by_page")}'
+			content_page['previous'] = re.sub(
+				r'^.+offset=(\d+)&limit=\d+$', 
+				r'https://pokeapi.co/api/v2/pokemon?offset=\g<1>&limit=' + str(data_paged.get('pokemons_by_page')), 
+				content_page.get('previous')
+			)
+		elif page_number == 1:
+			offset = data_paged.get('pokemons_by_page') * (data_paged.get('count_pages') - 1)	
+			content_page['previous'] = f'https://pokeapi.co/api/v2/pokemon?offset={offset}&limit={data_paged.get("count_pokemons") - offset}'
+			
+		page = {
+			'number':page_number, 
+			'offset':data_paged.get('pokemons_by_page') * (page_number - 1),
+			'next':content_page.pop('next'), 
+			'previous':content_page.pop('previous'), 
+			'content':content_page.pop('pokemons')
+		}
+	return page
 
 
 	# Pokedex sencilla:
@@ -42,3 +235,5 @@ def get_pokemons(limit=10, offset=0):
 	# SOLO ESTO NO COMPLIQUES EL EJEMPLO
 	# *** Tener en cuenta que count tiene el total de pokemons conocidos.
 	# *** También types contiene la lista de tipos a las que pertenece el pokemon, muestra el nombre.
+
+
